d208bf12143235e7022fd020fe159b3188447bb4
neeraj <neerajkumar04051998@gmail.com>
2025-08-18 13:25:24 +0530

    done

---
+++ src/Recursion/fibonacci.java
@@ -0,0 +1,18 @@
+package Recursion;
+public class fibonacci {
+    public static int fib(int n) {
+        if (n<=1) {
+            return n;
+        }
+        int last = fib(n-1);
+        int slast = fib(n-2);
+
+        return last+slast;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fib(n));
+    }
+}
+
